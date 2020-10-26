8fe64c6a2c759e672e04bfc081d0a8625f6656e2
Arvind Rangan <3113987+arangan@users.noreply.github.com>
2020-10-25 21:33:29 -0600

    Added another Unit test for determining the best discount or promotion if both exists

--- StoreDomain.Test/CartTest.cs
+++ StoreDomain.Test/CartTest.cs
@@ -66,7 +66,10 @@ namespace StoreDomain.Test
             Assert.Equal(2, receiptLineItems.Count);
             Assert.Equal(1, receipt.IgnoredItems.Count);
 
+            Assert.Equal(2141, receiptLineItems[0].PLU);
             Assert.Equal(1.99m, receiptLineItems[0].FinalPrice);
+
+            Assert.Equal(4011, receiptLineItems[1].PLU);
             Assert.Equal(0.89m, receiptLineItems[1].FinalPrice);
             Assert.Equal(2.88m, receipt.GrandTotal);
         }
@@ -114,6 +117,55 @@ namespace StoreDomain.Test
             Assert.Equal(1.98m, receiptLineItems[2].FinalPrice);
 
             Assert.Equal(26.03m, receipt.GrandTotal);
+        }
+
+        [Fact]
+        public void Can_Checkout_And_Get_Best_Sale_Price_Or_Promotion()
+        {
+            Dictionary<int, ISale> discounts = new Dictionary<int, ISale>(2);
+            discounts.Add(2141, new Sale(2141, 1.56m));
+            discounts.Add(4011, new Sale(4011, 0.89m));
+            _storeRepository.Setup(foo => foo.GetSalePrices()).Returns(discounts);
+
+            Dictionary<int, IPromotion> promotions = new Dictionary<int, IPromotion>(2);
+            promotions.Add(2141, new Promotion("GroupPromotionalPrice", 2141, 4, 0, 1.56m));
+            promotions.Add(4011, new Promotion("AdditionalProductDiscount", 4011, 3, 1, 100));
+            _storeRepository.Setup(fn => fn.GetPromotions()).Returns(promotions);
+
+            _cart = new Cart(_storeRepository.Object);
+            _cart.AddItem(new CartItem(2141, "Apple"));
+            _cart.AddItem(new CartItem(2141, "Apple"));
+            _cart.AddItem(new CartItem(2141, "Apple"));
+
+            _cart.AddItem(new CartItem(4011, "Banana"));
+            _cart.AddItem(new CartItem(4011, "Banana"));
+            _cart.AddItem(new CartItem(4011, "Banana"));
+            _cart.AddItem(new CartItem(4011, "Banana"));    
+
+            _cart.AddItem(new CartItem(3001, "Grapes"));
+            _cart.AddItem(new CartItem(3291, "Pineapple"));            
+            _cart.AddItem(new CartItem(2101, "Orange"));
+
+
+            Receipt receipt = _cart.Checkout();
+            List<ReceiptLineItem> receiptLineItems = receipt.GetLineItems().ToList();
+
+            Assert.Equal(4, receiptLineItems.Count);
+            Assert.Equal(1, receipt.IgnoredItems.Count);
+
+            Assert.Equal(2141, receiptLineItems[0].PLU);
+            Assert.Equal(4.68m, receiptLineItems[0].FinalPrice);
+
+            Assert.Equal(4011, receiptLineItems[1].PLU);
+            Assert.Equal(2.97m, receiptLineItems[1].FinalPrice);
+
+            Assert.Equal(3291, receiptLineItems[2].PLU);
+            Assert.Equal(4.55m, receiptLineItems[2].FinalPrice);
+
+            Assert.Equal(2101, receiptLineItems[3].PLU);
+            Assert.Equal(2.49m, receiptLineItems[3].FinalPrice);
+
+            Assert.Equal(14.69m, receipt.GrandTotal);
         }
     }
 }
